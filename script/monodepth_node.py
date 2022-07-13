1ade01424fe08b76abfea7df138be43512610fac
ramlabserver <yliuhb@connect.ust.hk>
2022-07-13 14:42:02 +0800

    add compressed image support

--- script/monodepth_node.py
+++ script/monodepth_node.py
@@ -6,7 +6,8 @@ import torch.nn.functional as F
 import onnxruntime as ort
 
 import rospy
-from sensor_msgs.msg import Image, CameraInfo, PointCloud2
+from sensor_msgs.msg import Image, CameraInfo, PointCloud2, CompressedImage
+from cv_bridge import CvBridge
 from ros_util import depth_image_to_point_cloud_array, publish_point_cloud, depth_image_to_point_cloud_tensor
 
 
@@ -36,17 +37,13 @@ class RosNode:
         import sys
         sys.path.append(monodepth_path)
         from lib.utils.utils import cfg_from_file
-        import lib.networks
-        import lib.data
-        import lib.pipeline_hooks
-        import lib.evaluation
 
         cfg_file = rospy.get_param("~CFG_FILE", "/home/yxliu/multi_cam/monodepth/configs/kitti360_gtpose_config.py")
         self.cfg = cfg_from_file(cfg_file)
         self.cfg.meta_arch.depth_backbone_cfg.pretrained=False
         # self.cfg.meta_arch.pose_backbone_cfg.pretrained=False
 
-        # self.onnx_path = rospy.get_param("~ONNX_PATH", "/home/yxliu/test_ws/model/monodepth.onnx")
+        self.onnx_path = rospy.get_param("~ONNX_PATH", "/home/yxliu/test_ws/model/monodepth.onnx")
         self.weight_path = rospy.get_param("~WEIGHT_PATH", "/home/yxliu/multi_cam/monodepth/workdirs/MonoDepth2WPose/checkpoint/MonoDepthWPose_ss11.pth")
 
         self.inference_w   = int(rospy.get_param("~INFERENCE_W",  640))
@@ -56,7 +53,7 @@ class RosNode:
         self.min_y = float(rospy.get_param("~MIN_Y", -2.0))
         self.max_y = float(rospy.get_param("~MAX_Y", 4.5))
 
-        self.cfg.data.test_augmentation.cfg_list[1].size = (self.inference_h, self.inference_w)
+        self.cfg.val_dataset.augmentation.cfg_list[1].size = (self.inference_h, self.inference_w)
 
     def _init_model(self):
         rospy.loginfo("Loading model.")
@@ -72,10 +69,9 @@ class RosNode:
         )
         self.meta_arch.load_state_dict(state_dict['model_state_dict'], strict=False)
         self.meta_arch.eval()
+        # self.ort_session = ort.InferenceSession(self.onnx_path, providers=['CUDAExecutionProvider'])
 
-        # self.ort_session = ort.InferenceSession(self.onnx_path)
-
-        self.transform = build(**self.cfg.data.test_augmentation)
+        self.transform = build(**self.cfg.val_dataset.augmentation)
         self.test_pipeline = build(**self.cfg.trainer.evaluate_hook.test_run_hook_cfg)
         rospy.loginfo("Done loading model.")
 
@@ -86,6 +82,7 @@ class RosNode:
     def _init_topics(self):
         self.pc_publisher = rospy.Publisher("/point_cloud", PointCloud2, queue_size=1)
         rospy.Subscriber("/image_raw", Image, self.camera_callback, buff_size=2**24, queue_size=1)
+        rospy.Subscriber("/compressed_image", CompressedImage, self.compressed_camera_callback, buff_size=2**24, queue_size=1)
         rospy.Subscriber("/camera_info", CameraInfo, self.camera_info_callback)
 
     def _predict_depth(self, image):
@@ -97,20 +94,16 @@ class RosNode:
         data = self.transform(data)
         data = collate_fn([data])
 
-        # import time
-        # start_time = time.time()
         # image_numpy = data[('image', 0)].contiguous().numpy()
         # outputs = self.ort_session.run(None, {'input': image_numpy})
         # depth = outputs[0][0, 0] * self.inference_scale 
-        # print(time.time() - start_time)
-        # np.clip(depth, 0, self.max_depth, out=depth)
 
         # depth = cv2.resize(
         #     depth, (w0, h0), interpolation=cv2.INTER_NEAREST
         # )
 
         # point_cloud = depth_image_to_point_cloud_array(depth, self.P[0:3, 0:3], rgb_image=image)
-        # mask = (point_cloud[:, 1] > self.min_y) * (point_cloud[:, 1] < self.max_y)
+        # mask = (point_cloud[:, 1] > self.min_y) * (point_cloud[:, 1] < self.max_y) * (point_cloud[:, 0] < self.max_depth)
         # point_cloud = point_cloud[mask]
         
         with torch.no_grad():
@@ -138,6 +131,12 @@ class RosNode:
             depth, point_cloud = self._predict_depth(image[:, :, ::-1].copy()) # BGR -> RGB
             publish_point_cloud(point_cloud, self.pc_publisher, self.frame_id, 'xyzrgb')
     
+    def compressed_camera_callback(self, msg):
+        if self.P is not None:
+            image = CvBridge().compressed_imgmsg_to_cv2(msg) #[BGR]
+            depth, point_cloud = self._predict_depth(image[:, :, ::-1].copy()) # BGR -> RGB
+            publish_point_cloud(point_cloud, self.pc_publisher, self.frame_id, 'xyzrgb')
+
     def camera_info_callback(self, msg):
         self.P = np.zeros((3, 4))
         self.P[0:3, 0:3] = np.array(msg.K).reshape((3, 3))
